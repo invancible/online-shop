eef2758fe6ec9fa7ca73b73304cf40033c11de18
Ivan Ramos <ivanramos.notdev@gmail.com>
2023-06-08 07:55:57 +0800

    add user & product model

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
+// TODO: add admin functionality
 app.use('/admin', adminRoutes);
 app.use(mainRoutes);
 
